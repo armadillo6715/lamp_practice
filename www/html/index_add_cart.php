231256eb1c84b933b015b9b7ea0c3bc54329661b
mitsumoto <alumazimo6715@gmail.com>
2020-09-23 09:50:38 +0900

    中途更新

--- www/html/index_add_cart.php
+++ www/html/index_add_cart.php
@@ -11,9 +11,17 @@ if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
 
+//トークン照合
+$token = get_post('token');
+
+if (is_valid_csrf_token($token) === false) {
+  redirect_to(LOGIN_URL);
+}
+//トークン破棄
+unset($_SESSION['csrf_token']);
+
 $db = get_db_connect();
 $user = get_login_user($db);
-
 
 $item_id = get_post('item_id');
 
@@ -23,9 +31,6 @@ if(add_cart($db,$user['user_id'], $item_id)){
   set_error('カートの更新に失敗しました。');
 }
 
-//トークン照合
-is_valid_csrf_token($token);
-//トークン破棄
-$_SESSION['csrf_token'] = '';
+
 
 redirect_to(HOME_URL);
